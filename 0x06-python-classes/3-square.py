5b80fef81c1542e7fe046b8405ec040dc63b9e82
Zoe Stocker <stockerazoe@gmail.com>
2022-06-21 03:10:04 -0700

    Area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A class Square that is based on 2-square.py. It has private instance attribute size and public instance method area. Returns current square area.
+
+"""
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size ** 2)
