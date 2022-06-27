cc4345ba60d0c4f2c49e004749dc9a03ef95d121
Zoe Stocker <stockerazoe@gmail.com>
2022-06-27 03:16:50 -0700

    Printing an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """
+    Printing an integer with "{:d}".format()
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
